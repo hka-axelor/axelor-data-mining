76a7dea362a48d5f9161e5f49a9639abb55c96af
hka-axelor <h.kapadia@axelor.com>
2022-06-06 12:35:56 +0530

    Automatically remove header from csv file

--- src/main/java/com/axelor/apps/data/mining/service/InvoiceAnalysisServiceImpl.java
+++ src/main/java/com/axelor/apps/data/mining/service/InvoiceAnalysisServiceImpl.java
@@ -1,21 +1,24 @@
 package com.axelor.apps.data.mining.service;
 
+import com.axelor.apps.tool.file.CsvTool;
 import com.axelor.meta.MetaFiles;
 import com.axelor.meta.db.MetaFile;
 import com.axelor.meta.db.repo.MetaFileRepository;
-import com.google.common.io.Files;
 import com.google.inject.Inject;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 public class InvoiceAnalysisServiceImpl implements InvoiceAnalysisService {
 
   public static final String PATH_TO_APRIORI_PYTHON_FILE =
       "/home/axelor/Projects/aos-dmbi-master/axelor-erp/modules/axelor-data-mining/src/main/resources/python/apriori.py";
+
+  public static final String INPUT_DATASET_NAME = "invoice_product_data.csv";
 
   protected MetaFileRepository metaFileRepo;
 
@@ -32,10 +35,16 @@ public class InvoiceAnalysisServiceImpl implements InvoiceAnalysisService {
 
     if (inputFileMap != null) {
       MetaFile dataFile = metaFileRepo.find(Long.parseLong(inputFileMap.get("id").toString()));
+      List<String[]> csvContent =
+          CsvTool.cSVFileReader(MetaFiles.getPath(dataFile).toString(), ',');
+      csvContent.remove(0); // Removing header
+
+      csvContent.sort(
+          (s1, s2) -> s2.length - s1.length); // Sorting in descending order based on size
 
       File tempDir = java.nio.file.Files.createTempDirectory(null).toFile();
-      File csvFile = new File(tempDir, "invoice_product_data.csv");
-      Files.copy(MetaFiles.getPath(dataFile).toFile(), csvFile);
+      File csvFile = new File(tempDir, INPUT_DATASET_NAME);
+      CsvTool.csvWriter(tempDir.getAbsolutePath(), INPUT_DATASET_NAME, ',', null, csvContent);
 
       String pathToInputFile = csvFile.getAbsolutePath();
 
